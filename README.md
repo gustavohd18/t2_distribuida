2516897d696264589ee731056783b9311fbb5f25
gustavohd18 <gustavo.hernandez@acad.pucrs.br>
2021-05-15 21:19:19 -0300

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Para rodar a app utilize o comando dart run bin/t2_distribution_programming.dart arg1 
-
+Na primeira vez que rodar caso nao tenha o dart antes instalado rode o comando dart run
 Created from templates made available by Stagehand under a BSD-style
 [license](https://github.com/dart-lang/stagehand/blob/master/LICENSE).
--- lib/supernodo/SuperNode.dart
+++ lib/supernodo/SuperNode.dart
@@ -3,13 +3,62 @@ import 'dart:io';
 import 'dart:math';
 import 'dart:typed_data';
 import 'dart:collection';
-
 import 'Messages.dart';
 
 class SuperNode {
   String name;
   HashMap<String, String> peersFiles = HashMap();
   SuperNode(this.name);
+
+  void handleConnectionMulticast(Socket client) {
+    print('Connection from'
+        ' ${client.remoteAddress.address}:${client.remotePort}');
+
+    client.listen(
+      (Uint8List data) async {
+        final message = String.fromCharCodes(data);
+        switch (message) {
+          case 'REQUEST_LIST_FILES':
+            {
+              // mandar requisicao de REQUEST_FILES_PEERS para o multicast
+              await sendPackageToMulticast('REQUEST_FILES_PEERS');
+              client.write('Solicitacao de arquivos atendidas');
+            }
+            break;
+
+          case 'REQUEST_PEER':
+            {
+              client.write('Solicitacao de arquivos atendidas');
+            }
+            break;
+
+          case 'SEND_FILES_LIST':
+            {
+              client.write('Solicitacao de arquivos atendidas');
+            }
+            break;
+
+          default:
+            {
+              client.write('Nada encontrado com essa solicitacao: ${message}');
+            }
+            break;
+        }
+      },
+
+      // handle errors
+      onError: (error) {
+        print(error);
+        client.close();
+      },
+
+      // handle the client closing the connection
+      onDone: () {
+        print('Conexao encerrada supernodo caiu');
+        client.close();
+      },
+    );
+  }
 
   void handleConnectionSocket(Socket client) {
     print('Connection from'
@@ -62,12 +111,12 @@ class SuperNode {
   }
 
   Future<void> sendPackageToMulticast(String message) {
-    InternetAddress multicastAddress = new InternetAddress('239.10.10.100');
-    int multicastPort = 4545;
-    RawDatagramSocket.bind(InternetAddress.ANY_IP_V4, 0)
-        .then((RawDatagramSocket s) {
-      print("mensagem multicast enviada $message");
-      stdout.write("Sending $message  \r");
+    var multicastAddress = InternetAddress('239.10.10.100');
+    var multicastPort = 4545;
+    var any_ip_v4 = InternetAddress.anyIPv4;
+    RawDatagramSocket.bind(any_ip_v4, 0).then((RawDatagramSocket s) {
+      stdout.write('Sending $message  \r');
+      print('mensagem multicast enviada $message');
       s.send('$message\n'.codeUnits, multicastAddress, multicastPort);
     });
   }
